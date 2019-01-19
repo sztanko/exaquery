90cf5a40f4615463fdb926638844ab8eb969a9d5
Demeter Sztanko <demeter.sztanko@revolut.com>
2019-01-19 23:15:21 +0000

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 cd app/backend
 api_url=${API_URL:-"/api/exaquery/"}
 echo "API URL IS $api_url"
-sed -i "s/__APIURL__/${api_url//\//\\/}/g" ../ui/build/static/js/*.js
+# This is MAC version
+#sed -i '' "s/__APIURL__/${api_url//\//\\/}/g" ../ui/build/static/js/*.js
+
+# This one is unix version
+sed -i "s~__APIURL__~${api_url}~g" ../ui/build/static/js/*.js
+
 HTML=../ui/build python3 run.py
