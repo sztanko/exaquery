f52817d87bbc749e282f8a110ff0395c2d39baac
Daniele Esposti <daniele.esposti@gmail.com>
2019-01-22 21:37:11 +0000

    Use non-blocking IO on production

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,11 @@
-from os import environ
 from os.path import abspath
+import os
+
 from werkzeug.wsgi import DispatcherMiddleware
 from flask import Flask, send_from_directory, send_file
+
 from timeline.timeline_app import app as timeline_app
+
 
 # set the project root directory as the static folder, you can set others.
 html_app = Flask(__name__, static_folder=None)  # , static_url_path="")
@@ -15,13 +18,19 @@ defaults = {"path": ""}
 def index(path):
     print(path)
     if path.startswith("static"):
-        p = abspath(environ.get("HTML") + "/" + path)
+        p = abspath(os.environ.get("HTML") + "/" + path)
         return send_file(p)
-    return send_from_directory(environ.get("HTML"), "index.html")
+    return send_from_directory(os.environ.get("HTML"), "index.html")
 
 
 app = Flask("main")
 app.wsgi_app = DispatcherMiddleware(html_app, {"/api": timeline_app})
+app.config.from_object(os.environ.get("SETTINGS", "settings.devel"))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=False)
+if app.config["DEBUG"]:
+    app.run(host=app.config["HOST"], port=app.config["PORT"], debug=True)
+else:
+    from gevent.pywsgi import WSGIServer
+
+    http_server = WSGIServer((app.config["HOST"], app.config["PORT"]), app)
+    http_server.serve_forever()
---
+++ backend/settings/prod.py
@@ -0,0 +1,3 @@
+HOST = "0.0.0.0"
+PORT = 5000
+DEBUG = False
